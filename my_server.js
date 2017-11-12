a8be229ee818e612f3bed9a75b7611400b76a729
alu0101069937 <32128127+alu0101069937@users.noreply.github.com>
2017-11-12 14:18:22 +0000

    Update my_server.js

--- my_server.js
+++ my_server.js
@@ -4,7 +4,7 @@ var server = http.createServer(function (peticion, respuesta){
   respuesta.end("Hola Web.com");
 });
 //"respuesta.end()" sirve para terminar la petición y enviar los datos al cliente.
-server.listen(3000, function(){
+server.listen(8080, function(){
   console.log("tu servidor está listo en " + this.address().port);
 });
 // Ahora le decimos al servidor que se ponga en marcha.
