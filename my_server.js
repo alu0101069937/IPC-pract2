c697763f1b0a7096e454f9b714893fc3b6cab1b3
alu0101069937 <32128127+alu0101069937@users.noreply.github.com>
2017-11-12 14:41:44 +0000

    Update my_server.js

--- my_server.js
+++ my_server.js
@@ -1,3 +1,23 @@
+// importar
+var express = require('express');
+
+// instanciar
+var app = express();
+
+// ruteo
+app.get('/', function(req, res){
+res.sendfile(__dirname + '/public/mipagina.html');
+});
+app.get('/about', function(req, res){
+res.sendfile(__dirname + '/public/mipagina.html');
+});
+
+// escuchar puerto 8080
+app.listen(8080);
+
+console.log("Servidor Express escuchando en modo %s", app.settings.env);
+
+/*===FICHERO INICIAL DE PRUEBAS=====
 //Indica que queremos trabajar con HTTP (tb se podría con “net”,”tcp”,”tls”)
 var http = require("http"); 
 var server = http.createServer(function (peticion, respuesta){
@@ -8,3 +28,4 @@ server.listen(8080, function(){
   console.log("tu servidor está listo en " + this.address().port);
 });
 // Ahora le decimos al servidor que se ponga en marcha.
+*/
