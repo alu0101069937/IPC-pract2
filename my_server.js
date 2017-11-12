8970114d3e15ef5e2e57eb4ffb7af5bc61cc738d
alu0101069937 <32128127+alu0101069937@users.noreply.github.com>
2017-11-12 16:42:52 +0000

    Update my_server.js

--- my_server.js
+++ my_server.js
@@ -3,7 +3,7 @@ var express = require('express');
 
 // instanciar
 var app = express();
-app.use(express.static(path.join(__dirname, 'public')));
+
 // ruteo
 app.get('/', function(req, res){
 res.sendfile(__dirname + '/public/mipagina.html');
@@ -18,20 +18,7 @@ app.listen(8080);
 console.log("Servidor Express escuchando en modo %s", app.settings.env);
 
 /*===FICHERO INICIAL DE PRUEBAS=====
-var express = require('express');
-var path = require('path');
-var app = express();
 
-// Define the port to run on
-app.set('port', 3000);
-
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Listen for requests
-var server = app.listen(app.get('port'), function() {
-  var port = server.address().port;
-  console.log('Magic happens on port ' + port);
-});
 ==============
 //Indica que queremos trabajar con HTTP (tb se podría con “net”,”tcp”,”tls”)
 var http = require("http"); 
