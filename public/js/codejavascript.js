4c5c71bc71ddb4ad6509a5f741fe6df53cf1010e
alu0101069937 <32128127+alu0101069937@users.noreply.github.com>
2017-12-13 17:49:11 +0000

    Update codejavascript.js

--- public/js/codejavascript.js
+++ public/js/codejavascript.js
@@ -1,1 +1,8 @@
-
+function validarEmail(email) {
+  var comparaEmail = /^(\w{2,})+@{1}(\w{3,})+\.(\w{2,})$/;
+	if(!email.match(comparaEmail)) {
+	  alert("Dirección de correo NO válida.\nUse formato ab+@abc+.ab+");
+	  return false;
+	  }	  
+	}
+// Prohibido campos 'nombre' y 'passwd' VACÍOS --> con la etiqueta REQUIRED en el html.
